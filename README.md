b35d5354f95c98e970f3f2523d939ff0ad76ab68
Matt Payne <payne@mattpayne.org>
2016-12-26 18:06:11 +0000

    blank things out from master and start with release

--- README.md
+++
@@ -1,2 +0,0 @@
-# intro-to-browserify
-The companion code for the course "Intro to Browserify."
--- src/display_owners.js
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash';
-import ownerTemplate from './owner_template.hbs';
-
-const ownerContainer = document.getElementById('owners');
-
-export default owners => {
-  ownerContainer.innerHTML += _.map(owners, ownerTemplate).join('');
-};
