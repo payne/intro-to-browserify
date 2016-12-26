bc7a8c54bb77c36917aa1ca4c21745962d568e97
Matt Payne <payne@mattpayne.org>
2016-12-26 21:41:19 +0000

    npm install --save-dev browserify babelify babel-core babel-preset-es2015

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-(function($, _, displayOwners) {
+import $ from 'jquery';
+import _ from 'lodash';
+import displayOwners from './display_owners'
+
   const startAt = _.random(1, 5000);
 
-  $.get(`https://api.github.com/repositories?since=${startAt}`, repos => {
-    const allOwners = _.map(repos, 'owner');
-    const owners = _.uniqBy(allOwners, 'id');
+$.get(`https://api.github.com/repositories?since=${startAt}`, repos => {
+  const allOwners = _.map(repos, 'owner');
+  const owners = _.uniqBy(allOwners, 'id');
 
-    displayOwners(owners);
-  });
-})(jQuery, _, displayOwners);
+  displayOwners(owners);
+});
--- src/display_owners.js
+++ src/display_owners.js
@@ -1,12 +1,14 @@
-window.displayOwners = (function($, _, Handlebars) {
-  const ownerContainer = $('#owners');
-  const ownerSource = $('#ownerTemplate').html();
-  const ownerTemplate = Handlebars.compile(ownerSource);
+import $ from 'jquery';
+import _ from 'lodash';
+import Handlebars from 'handlebars';
 
-  return owners => {
-    _.forEach(owners, owner => {
-      const ownerCompiled = ownerTemplate(owner);
-      ownerContainer.append(ownerCompiled);
-    });
-  };
-})(jQuery, _, Handlebars);
+const ownerContainer = $('#owners');
+const ownerSource = $('#ownerTemplate').html();
+const ownerTemplate = Handlebars.compile(ownerSource);
+
+export default owners => {
+  _.forEach(owners, owner => {
+    const ownerCompiled = ownerTemplate(owner);
+    ownerContainer.append(ownerCompiled);
+  });
+};
